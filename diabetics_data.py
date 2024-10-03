d5dbd4c447ee837f4175f155b797c0642a7fe194
sivanandu-03 <149658969+sivanandu-03@users.noreply.github.com>
2024-10-03 20:05:45 +0700

    diabetics_data.py

--- diabetics_data.py
+++ diabetics_data.py
@@ -10,7 +10,7 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetics_model.sav', 'rb'))
 # sidebar for navigation
 with st.sidebar:
     
